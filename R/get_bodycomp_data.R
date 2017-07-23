44f0fe55df373bcbe287aa17f3acb9b40c06c8ad
Elliot Palmer <palm1221ks@gmail.com>
2017-07-23 14:00:09 -0400

    Update get_bodycomp_data.R
    adding documentation updates

--- R/get_bodycomp_data.R
+++ R/get_bodycomp_data.R
@@ -1,4 +1,31 @@
+#' Get body comp data from fitbit.com
+#'
+#' Get body comp data from fitbit using cookie returned from login function
+#' @param cookie Cookie returned after login, specifically the "u" cookie
+#' @param start_date Date in YYYY-MM-DD format
+#' @param end_date Date in YYYY-MM-DD format
+#' @keywords data
+#' @export
+#' @return A dataframe with five columns:
+#'  \item{time}{A POSIXct time value}
+#'  \item{lean}{The data column corresponding to lean body mass}
+#'  \item{weight}{The data column corresponding to weight}
+#'  \item{fat}{The data column corresponding to fat body mass}
+#'  \item{bf_pct}{The data column corresponding to body fat percent}
+#' @examples
+#' \dontrun{
+#' get_bodycomp_data(cookie, start_date="2015-01-13", end_date="2015-01-20")
+#' }
+#' get_bodycomp_data
+
 get_bodycomp_data <- function(cookie, start_date,end_date){
+  
+  if(!is.character(cookie)){stop("cookie must be a character string")}
+  if(!is.character(start_date)){stop("start_date must be a character string")}
+  if(!is.character(end_date)){stop("end_date must be a character string")}
+  if(!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", start_date)){stop('start_date must have format "YYYY-MM-DD"')}
+  if(!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", end_date)){stop('end_date must have format "YYYY-MM-DD"')}  
+  
   url <- "https://www.fitbit.com/graph/getNewGraphData"
   query <- list(type = "bodyComp", dateFrom = start_date, dateTo = end_date)
   response <- httr::GET(url, query = query, httr::config(cookie = cookie))
@@ -17,3 +44,5 @@ get_bodycomp_data <- function(cookie, start_date,end_date){
   df$bf_pct <- df$fat / df$weight
   return(df)
 }
+
+
